9099ba43beb2579bc8b3c95a6c531413f304f9d7
SyemAbedin <Syema@nycstudents.net>
2025-11-20 15:33:09 -0500

    almost done, needs iteration

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        File f = new File("Scoreboard.txt");
+        Scanner s = new Scanner(f);
+    }
 }
--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,2 +1,26 @@
 public class Scoreboard {
+    String team1 = "";
+    String team2 = "";
+    static int points1 = 0;
+    static int points2 = 0;
+    static boolean firstTeam = true;
+    public void constructor(String name1, String name2) {
+        team1 = name1;
+        team2 = name2;
+    }
+    public static void recordPlay(int pnt) {
+        if (pnt > 0) {
+            if (firstTeam) {
+                points1 = points1 + pnt;
+            } else {
+                points2 = points2 + pnt;
+            }
+        } else {
+            firstTeam = !firstTeam;
+        }
+    }
+    public String getScore() {
+        if (points1 > points2) return points1 + "-" + points2 + "-" + team1;
+        return points1 + "-" + points2 + "-" + team2;
+    }
 }
